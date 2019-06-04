ea57f275d23edc56362845377b78cbffd1c02efa
alex fox <to.alexfox@gmail.com>
2019-06-04 23:21:33 +0200

    add DirectionsRenderer

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,24 +1,26 @@
 import { compose, withProps } from "recompose"
 import React, {Component} from "react"
-import { withGoogleMap, withScriptjs, GoogleMap, Marker } from "react-google-maps"
+import { withGoogleMap, GoogleMap, DirectionsRenderer } from "react-google-maps"
 
-const Map = compose(
-	withProps({
-					googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBAFajUxQ7Ltv5t9nfiaYTXvhnWbTV80bk&libraries=places",
-					loadingElement: <div style={{ height: `100%` }} />,
-					containerElement: <div style={{ height: `400px` }} />,
-					mapElement: <div style={{ height: `100%` }} />}),
-	withGoogleMap)((props) => 
-	{
+var directionsService = new window.google.maps.DirectionsService;
+
+class DisplayMap extends Component { 
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		console.log("display map props: ", this.props)
 		return(
 			<div className="map">
 				<GoogleMap
 					defaultZoom={8}
 					defaultCenter={{ lat: -34.397, lng: 150.644 }}
 				>
+					{this.props.map.map && <DirectionsRenderer geocoded_waypoints={this.props.map.map.waypoints} />}
 				</GoogleMap>
 			</div>
-	)})
+	)}
+}
 
-
-export default Map
+export default DisplayMap
--- src/pages/View-Map.jsx
+++ src/pages/View-Map.jsx
@@ -2,28 +2,48 @@ import React, {Component} from "react"
 import { getOneMap } from "../api/apiHandler"
 import DisplayMap from "../components/Map"
 
+var directionsService = new window.google.maps.DirectionsService;
+
 class ViewMap extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			map_id: props.match.params.id,
 			map: {},
+			route: [],
 		};
 	}
 
 	async componentDidMount() {
 		var thisMap = await getOneMap(this.props.match.params.id)
 		thisMap = thisMap.data.map
+
 		this.setState({
-			map: thisMap
+			map: thisMap,
+			route: this.calculateRoute(directionsService, thisMap.map)
 		})
+	}
+
+	calculateRoute( service, object ) {
+		service.route(object, function(response, status) {
+			if (status === 'OK') {
+				console.log("response: ", response)
+				return response
+			} else {
+				window.alert('Directions request failed due to ' + status);
+			}
+		});
 	}
 
 	render() {
 		console.log("map page state:, ", this.state)
 		return (
 			<div className="body-container">
-				<DisplayMap />
+				<DisplayMap map={this.state.map} 
+					loadingElement = {<div style={{ height: `100%` }} />}
+					containerElement= {<div style={{ height: `400px` }} />}
+					mapElement= {<div style={{ height: `100%` }} />}
+				/>
 				{this.state.map && 
 				<div className="mapInfo">
 					Total Time: {this.state.map.total_time}
