daaa4445d7c074853b6f8582384fc713055ece65
alex fox <to.alexfox@gmail.com>
2019-06-22 16:48:38 -0500

    add react DnD

---
+++ src/components/DraggableList.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import { DndProvider } from 'react-dnd'
+import HTML5Backend from 'react-dnd-html5-backend'
+
+class DnDList extends Component {
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		return (
+			<div>
+
+			</div>
+		)
+	}
+}
